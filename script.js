4ef9bc94cf305828e7eb6729aec2da86bdabb42d
Glitch (glitch-blank-website) <none>
2023-01-27 21:09:17 +0000

    🚁🌲 Checkpoint ./index.html:18914280/1633 ./script.js:18914280/4603 ./style.css:18914280/993

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
 const startButton = document.getElementById("start-button");
-const stopButton = document.getElementById("stop-button");
-const timerDisplay = document.getElementById("timer");
 const resetButton = document.getElementById("reset-button");
-
-
-
+const timerDisplay = document.getElementById("timer");
 
 let countdown;
 let timeLeft = 1500;
 let timerRunning = false;
-
-function startTimer() {
-  if (!timerRunning) {
-    countdown = setInterval(() => {
-      timeLeft--;
-      let minutes = Math.floor(timeLeft / 60);
-      let seconds = timeLeft % 60;
-      if(seconds < 10) {
-        seconds = `0${seconds}`;
-      }
-      timerDisplay.innerHTML = `${minutes}:${seconds}`;
-      if (timeLeft === 0) {
-        clearInterval(countdown);
-      }
-    }, 1000);
-    startButton.innerHTML = "Parar";
-    timerRunning = true;
-  } else {
-    clearInterval(countdown);
-    startButton.innerHTML = "Iniciar";
-    timerRunning = false;
-  }
-}
-
-startButton.addEventListener("click", toggleTimer);
-resetButton.addEventListener("click", resetTimer);
 
 function toggleTimer() {
     if (timerRunning) {
@@ -77,3 +47,13 @@ function resetTimer() {
     resetButton.style.display = "none";
     timerRunning = false;
 }
+
+startButton.addEventListener("click", toggleTimer);
+resetButton.addEventListener("click", resetTimer);
+
+let minutes = Math.floor(timeLeft / 60);
+let seconds = timeLeft % 60;
+if(seconds < 10) {
+  seconds = `0${seconds}`;
+}
+timerDisplay.innerHTML = `${minutes}:${seconds}`;
