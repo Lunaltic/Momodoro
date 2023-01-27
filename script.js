30b874a01e526b0ae0d347e9905aecebfe7807d5
Glitch (glitch-blank-website) <none>
2023-01-27 22:20:32 +0000

    🙍🐉 Checkpoint ./script.js:18914280/3270 ./index.html:18914280/1280

--- script.js
+++ script.js
@@ -1,53 +1,38 @@
-  const startButton = document.getElementById("start-button");
-    const resetButton = document.getElementById("reset-button");
-    const timerDisplay = document.getElementById("timer");
+const startButton = document.getElementById("start-button");
+const resetButton = document.getElementById("reset-button");
+const timerDisplay = document.getElementById("timer");
 
-    let countdown;
-    let timeLeft = 1500;
-    let timerRunning = false;
+let intervalId;
+let timeLeft = 1500; // 25 minutos em segundos
 
-    function toggleTimer() {
-        if (timerRunning) {
-            clearInterval(countdown);
-            startButton.innerHTML = "Iniciar";
-            timerRunning = false;
-            resetButton.style.display = "block";
-        } else {
-            countdown = setInterval(() => {
-                timeLeft--;
-                let minutes = Math.floor(timeLeft / 60);
-                let seconds = timeLeft % 60;
-                if(seconds < 10) {
-                    seconds = `0${seconds}`;
-                }
-                timerDisplay.innerHTML = `${minutes}:${seconds}`;
-                if (timeLeft === 0) {
-                    clearInterval(countdown);
-                    timerRunning = false;
-                    startButton.innerHTML = "Iniciar";
-                    resetButton.style.display = "block";
-                }
-            }, 1000);
-            startButton.innerHTML = "Parar";
-            timerRunning = true;
-            resetButton.style.display = "none";
+startButton.addEventListener("click", startTimer);
+resetButton.addEventListener("click", resetTimer);
+
+function startTimer() {
+    if (intervalId) return; // impede que o cronômetro seja iniciado novamente se já estiver rodando
+    intervalId = setInterval(() => {
+        if (timeLeft <= 0) {
+            clearInterval(intervalId);
+            intervalId = null;
+            return;
         }
+        timeLeft--;
+        updateTimerDisplay();
+    }, 1000);
+}
+
+function resetTimer() {
+    clearInterval(intervalId);
+    intervalId = null;
+    timeLeft = 1500;
+    updateTimerDisplay();
+}
+
+function updateTimerDisplay() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
     }
-
-    function resetTimer() {
-        clearInterval(countdown);
-        timeLeft = 1500;
-        let minutes = Math.floor(timeLeft / 60);
-        let seconds = timeLeft % 60;
-        if(seconds < 10) {
-            seconds = `0${seconds}`;
-        }
-        timerDisplay.innerHTML = `${minutes}:${seconds}`;
-        startButton.innerHTML = "Iniciar";
-        resetButton.style.display = "none";
-        timerRunning = false;
-    }
-
-    startButton.addEventListener("click", toggleTimer);
-    resetButton.addEventListener("click", resetTimer);
-
+    timerDisplay.innerHTML = `${minutes}:${seconds}`;
+}
