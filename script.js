902b935398128344fd322c744390221c99b3661a
Glitch (glitch-blank-website) <none>
2023-01-27 20:46:15 +0000

    🕍✋ Checkpoint ./index.html:18914280/209 ./style.css:18914280/145 ./script.js:18914280/9288

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello, world!");
+const startButton = document.getElementById("start-button");
+const stopButton = document.getElementById("stop-button");
+const timerDisplay = document.getElementById("timer");
+
+let countdown;
+let timeLeft = 1500;
+let timerRunning = false;
+
+function startTimer() {
+  if (!timerRunning) {
+    countdown = setInterval(() => {
+      timeLeft--;
+      let minutes = Math.floor(timeLeft / 60);
+      let seconds = timeLeft % 60;
+      if(seconds < 10) {
+        seconds = `0${seconds}`;
+      }
+      timerDisplay.innerHTML = `${minutes}:${seconds}`;
+      if (timeLeft === 0) {
+        clearInterval(countdown);
+      }
+    }, 1000);
+    startButton.innerHTML = "Parar";
+    timerRunning = true;
+  } else {
+    clearInterval(countdown);
+    startButton.innerHTML = "Iniciar";
+    timerRunning = false;
+  }
+}
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", () => clearInterval(countount));
